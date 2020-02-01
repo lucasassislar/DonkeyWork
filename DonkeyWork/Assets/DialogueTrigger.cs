408f6036fdfa06dee2133be60e9f4e763afd3051
Ana <ana@pixelripped.com>
2020-02-01 00:46:55 -0300

    dialogo prefab

---
+++ DonkeyWork/Assets/DialogueTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class DialogueTrigger : MonoBehaviour
+{
+    public UnityEvent Activatedialogue;
+    public UnityEvent Deactivatedialogue;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider c)
+    {
+        Debug.Log("colidio");
+        if (c.transform.tag == "Player")
+        {
+            Activatedialogue.Invoke();
+            Debug.Log("colidio");
+        }
+     
+    }
+    void OnTriggerExit(Collider c)
+    {
+        if (c.transform.tag == "Player")
+        {
+            Deactivatedialogue.Invoke();
+        }
+
+    }
+
+
+
+}
