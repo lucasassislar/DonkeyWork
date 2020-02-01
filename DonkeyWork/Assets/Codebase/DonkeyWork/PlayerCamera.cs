ac5b5942121c25d8892324369e23c39145c0c9f1
Lucas Assis <lucasassis@outlook.com>
2020-02-01 02:01:17 -0300

    vish

---
+++ DonkeyWork/Assets/Codebase/DonkeyWork/PlayerCamera.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DonkeyWork {
+    public class PlayerCamera : MonoBehaviour {
+        /// <summary>
+        /// A reference to the player on the scene
+        /// </summary>
+        public PlayerController playerController;
+
+        public float fCameraSpeed = 6.0f;
+
+        private Vector3 vOffsetPos;
+
+        private void Start() {
+            vOffsetPos = transform.position - playerController.transform.position;
+        }
+
+        private void LateUpdate() {
+            Vector3 vTargetPos = playerController.transform.position + this.vOffsetPos;
+            transform.position = Vector3.Lerp(transform.position, vTargetPos, fCameraSpeed * Time.deltaTime);
+        }
+    }
+}
