ce15385b55adad73d17b41a33bb3f5369b694e2f
Lucas Assis <lucasassis@outlook.com>
2020-02-01 00:45:22 -0300

    player controller

--- DonkeyWork/Assets/Codebase/DonkeyWork/PlayerController.cs
+++ DonkeyWork/Assets/Codebase/DonkeyWork/PlayerController.cs
@@ -1,17 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-namespace PlayerController {
+namespace DonkeyWork {
     public class PlayerController : MonoBehaviour {
-        // Start is called before the first frame update
-        void Start() {
+        private CharacterController playerController;
 
+        public float fGravity = -10;
+
+        public PlayerWorldState WorldState { get; private set; }
+
+        void Start() {
+            playerController = GetComponent<CharacterController>();
+
+            WorldState = new PlayerWorldState();
         }
 
-        // Update is called once per frame
         void Update() {
+            Vector3 vMovement = new Vector3();
+            WorldState.MovementY -= fGravity * Time.deltaTime;
 
+            Keyboard keyboard = Keyboard.current;
+            if (keyboard == null) {
+                return;
+            }
+
+            float fXMovement = 0;
+            if (keyboard.leftArrowKey.isPressed) {
+                fXMovement = 1;
+            } else if (keyboard.rightArrowKey.isPressed) { 
+                fXMovement -= 1;
+            }
+
+            vMovement.x = fXMovement;
+            vMovement.y = WorldState.MovementY;
+
+            playerController.Move(vMovement);
         }
     }
 }
---
+++ DonkeyWork/Assets/Codebase/DonkeyWork/PlayerWorldState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonkeyWork {
+    public class PlayerWorldState {
+        public float MovementY { get; set; }
+    }
+}
