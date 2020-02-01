01445511e1103d26bbb48df92873db3cc9ab0e40
Ana <ana@pixelripped.com>
2020-02-01 20:15:20 -0300

    hauhau

---
+++ DonkeyWork/Assets/Codebase/PlayRandomSound.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine;
+using System.Collections;
+
+public class PlayRandomSound : MonoBehaviour
+{
+				public AudioClip[] clipList;
+			
+				public void PlayAudio()
+				{
+                int rand = Random.Range(0, 6000) % clipList.Length;
+                GetComponent<AudioSource>().clip = clipList[rand];
+                GetComponent<AudioSource>().Play();
+    }
+}
