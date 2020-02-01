7da24418d9f691aa2d9485747925fadaf24d2f77
Lucas Assis <lucasassis@outlook.com>
2020-02-01 17:26:41 -0300

    gametasks plei

---
+++ DonkeyWork/Assets/Codebase/GameTasks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DonkeyWork {
+    public class GameTasks : MonoBehaviour {
+
+        public void EnableKinChildren(Transform parent) {
+            foreach (Transform tr in parent) {
+                Rigidbody body = tr.GetComponent<Rigidbody>();
+                if (!body) {
+                    continue;
+                }
+
+                body.isKinematic = false;
+            }
+        }
+    }
+}
